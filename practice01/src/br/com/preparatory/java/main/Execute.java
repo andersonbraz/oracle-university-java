68e7179e2a61be2caaf9c900feed8a29bce6f139
Anderson Braz <braz_anderson@msn.com>
2021-03-10 21:36:19 -0300

    Up file(s)

---
+++ practice01/src/br/com/preparatory/java/main/Execute.java
@@ -0,0 +1,9 @@
+package br.com.preparatory.java.main;
+
+public class Execute {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ practice01/src/br/com/preparatory/java/model/DepartmentFinder.java
@@ -0,0 +1,5 @@
+package br.com.preparatory.java.model;
+
+public class DepartmentFinder {
+
+}
